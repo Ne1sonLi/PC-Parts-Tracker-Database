30d7fd9ae3b7ac23d6eecbc80b9b1739f4f2f0de
nchan315 <nchan315@cs.students.ubc.ca>
2023-11-29 20:57:08 -0800

    still thinking about div

--- template.php
+++ template.php
@@ -674,6 +674,53 @@ echo "</table>";
 
 </div> -->
 
+
+	<!-- Division  -->
+	<div class=table-continer>
+		<h2>Find Cases With All Different Colours of Fans</h2>
+		<form method="post">
+    		<button type="submit" name="divButton">Find Cases</button>
+		</form>
+
+		<h2>Cases Table</h2>
+		<?php
+			if (isset($_POST['divButton'])) {
+				// create a view
+				// $view = "CREATE VIEW Colours(colour) AS SELECT DISTINCT Colour FROM CaseFan_Inside";
+				// executePlainSQL($view);
+				
+				// $sql = "SELECT * FROM Case_Contains C WHERE NOT EXISTS
+				// 			(SELECT DISTINCT CF.colour FROM CaseFan_Inside CF WHERE NOT EXISTS
+				// 				(SELECT CC.Model, CC.Colour, CC.Case_Size, CC.Price FROM CaseFan_Inside CC
+				// 					WHERE CF.colour = CC.colour
+				// 					AND CC.Model = C.Model
+				// 					AND CC.Colour = C.Colour
+				// 					AND CC.Case_Size = C.Case_Size))";
+				$sql = "SELECT *
+				FROM Case_Contains CC
+				WHERE NOT EXISTS (
+					SELECT CF2.CaseFan_Size
+					FROM CaseFan_Inside CF2
+					WHERE CC.Model = CF2.Case_Model
+						AND CC.Colour = CF2.Case_Colour
+						AND CC.Case_Size = CF2.Case_Size
+					MINUS
+					SELECT CF.CaseFan_Size
+					FROM CaseFan_Inside CF
+					WHERE CC.Model = CF.Case_Model
+						AND CC.Colour = CF.Case_Colour
+						AND CC.Case_Size = CF.Case_Size
+				)";
+			} else {
+				$sql = "SELECT * FROM CaseFan_Inside";
+			}
+			$result = executePlainSQL($sql);
+			echo "<table border='5'>";
+			printCPUCoolerTable($result);
+			echo "</table>";
+		?>
+	</div>
+
 	<!-- <h2>Count the Tuples in DemoTable</h2>
 	<form method="GET" action="template.php">
 		<input type="hidden" id="countTupleRequest" name="countTupleRequest">
